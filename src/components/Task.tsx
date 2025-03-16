2b91f6e96895253f890431ce3114b22d7eae6f1c
Kristaps-M <kristapsmitins@gmail.com>
2025-03-16 15:37:38 +0200

    fix text and background color in Task.tsx is white when Dark theme!

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -24,7 +24,7 @@ const Task: React.FC<TaskProps> = ({ task, handleDeleteJob, handleEditJob }) => 
         textAlign: "center",
         alignItems: "center",
         backgroundColor: task?.status === "done" ? "gray" : "white",
-        color: task?.status === "done" && "white",
+        color: task?.status === "done" ? "white" : "black",
         fontWeight: "bold",
         // gap: "10px",
         opacity: isDragging ? 0.5 : 1,
