64abff2c70116d73a4b4645432d44122f56e86a5
Kristaps-M <kristapsmitins@gmail.com>
2025-03-19 00:07:29 +0200

    create simple click logic!

--- src/components/CardMemoryGame/CardMemoryGame.tsx
+++ src/components/CardMemoryGame/CardMemoryGame.tsx
@@ -3,6 +3,8 @@ import TheTimer from "./TheTimer";
 import { IGameDiv } from "../../interfaces/IGameDiv";
 import { IFieldSize } from "../../interfaces/IFieldSize";
 import { gameFieldBackEnd } from "./GameDiv";
+let countOpenedCells = 0;
+let listOfClickedCell = [];
 
 const CardMemoryGame = () => {
   const [fieldSize, setFieldSize] = useState<IFieldSize>({ w: 5, h: 5 });
@@ -119,60 +121,85 @@ function handleSomethingWithList(
   id: string,
   setIsMachedPair: Dispatch<SetStateAction<string>>
 ) {
+  // condition to allow open?!
+  let clickedCell = { v: -1, h: -1, w: -1 };
+  // let openedCells = { a: 9999, b: 9999, ha: 0, wa: 0, hb: 0, wb: 0 };
   const theH = parseInt(id.split("-")[0]);
   const theW = parseInt(id.split("-")[1]);
-  const tempList = [...l];
-  tempList[theH][theW].isOpened = true;
-  setRealGameFieldBackEnd([...tempList]);
-  let countOpenedCells = 0;
-  let openedCells = { a: 9999, b: 9999, ha: 0, wa: 0, hb: 0, wb: 0 };
-  for (let theHeight = 0; theHeight < size.h; theHeight++) {
-    for (let theWidth = 0; theWidth < size.w; theWidth++) {
-      if (l[theHeight][theWidth].isOpened && !l[theHeight][theWidth].isGuessed) {
-        if (countOpenedCells === 0) {
-          openedCells.a = l[theHeight][theWidth].theValue;
-          openedCells.ha = theHeight;
-          openedCells.wa = theWidth;
-        } else if (countOpenedCells === 1) {
-          openedCells.b = l[theHeight][theWidth].theValue;
-          openedCells.hb = theHeight;
-          openedCells.wb = theWidth;
-        }
-        countOpenedCells++;
-      }
-    }
+  if (countOpenedCells === 0) {
+    const tempList = [...l];
+    tempList[theH][theW].isOpened = true;
+    clickedCell = { v: tempList[theH][theW].theValue, h: theH, w: theW };
+    listOfClickedCell.push(clickedCell);
+    setRealGameFieldBackEnd([...tempList]);
+    countOpenedCells++;
+    console.log(listOfClickedCell);
+  } else if (
+    countOpenedCells === 1 &&
+    // prevents from clicking same cell as first time!
+    !(listOfClickedCell[0].h === theH && listOfClickedCell[0].w === theW)
+  ) {
+    console.log("second click!", listOfClickedCell, theH, theW);
+    const tempList = [...l];
+    tempList[theH][theW].isOpened = true;
+    clickedCell = { v: tempList[theH][theW].theValue, h: theH, w: theW };
+    listOfClickedCell.push(clickedCell);
+    setRealGameFieldBackEnd([...tempList]);
+    countOpenedCells++;
+    listOfClickedCell = [];
   }
+  console.log("START - countOpenedCells", countOpenedCells, clickedCell);
 
-  for (let theHeight = 0; theHeight < size.h; theHeight++) {
-    for (let theWidth = 0; theWidth < size.w; theWidth++) {
-      if (openedCells.a === openedCells.b && openedCells.a !== 9999) {
-        const tempField = [...l];
-        tempField[openedCells.ha][openedCells.wa].isGuessed = true;
-        tempField[openedCells.hb][openedCells.wb].isGuessed = true;
-        tempField[openedCells.ha][openedCells.wa].isOpened = true;
-        tempField[openedCells.hb][openedCells.wb].isOpened = true;
-        setIsMachedPair("You Found A Pair! Congrats!" + `< ${openedCells.a} >`);
-        openedCells = { a: 9999, b: 9999, ha: 0, wa: 0, hb: 0, wb: 0 };
-        countOpenedCells = 0;
-        setRealGameFieldBackEnd([...tempField]);
-        alert("You have found 2 matching Cards!");
-      } else if (openedCells.a !== openedCells.b && countOpenedCells !== 1) {
-        const tempField = [...l];
-        setIsMachedPair("PAIR NOT FOUND!!!");
-        setTimeout(function () {
-          if (
-            tempField[openedCells.ha][openedCells.wa].isGuessed === false &&
-            tempField[openedCells.hb][openedCells.wb].isGuessed === false
-          ) {
-            tempField[openedCells.ha][openedCells.wa].isOpened = false;
-            tempField[openedCells.hb][openedCells.wb].isOpened = false;
-          }
-          openedCells = { a: 9999, b: 9999, ha: 0, wa: 0, hb: 0, wb: 0 };
-          countOpenedCells = 0;
-          setRealGameFieldBackEnd([...tempField]);
-        }, 2000);
-      }
-    }
-  }
+  // for (let theHeight = 0; theHeight < size.h; theHeight++) {
+  //   for (let theWidth = 0; theWidth < size.w; theWidth++) {
+  //     if (l[theHeight][theWidth].isOpened && !l[theHeight][theWidth].isGuessed) {
+  //       if (countOpenedCells === 0) {
+  //         openedCells.a = l[theHeight][theWidth].theValue;
+  //         openedCells.ha = theHeight;
+  //         openedCells.wa = theWidth;
+  //         countOpenedCells++;
+  //       } else if (countOpenedCells === 1) {
+  //         openedCells.b = l[theHeight][theWidth].theValue;
+  //         openedCells.hb = theHeight;
+  //         openedCells.wb = theWidth;
+  //         countOpenedCells++;
+  //       }
+  //       // countOpenedCells++;
+  //     }
+  //   }
+  // }
+  // console.log("MID - countOpenedCells", countOpenedCells);
+
+  // for (let theHeight = 0; theHeight < size.h; theHeight++) {
+  //   for (let theWidth = 0; theWidth < size.w; theWidth++) {
+  //     if (openedCells.a === openedCells.b && openedCells.a !== 9999) {
+  //       const tempField = [...l];
+  //       tempField[openedCells.ha][openedCells.wa].isGuessed = true;
+  //       tempField[openedCells.hb][openedCells.wb].isGuessed = true;
+  //       tempField[openedCells.ha][openedCells.wa].isOpened = true;
+  //       tempField[openedCells.hb][openedCells.wb].isOpened = true;
+  //       setIsMachedPair("You Found A Pair! Congrats!" + `< ${openedCells.a} >`);
+  //       openedCells = { a: 9999, b: 9999, ha: 0, wa: 0, hb: 0, wb: 0 };
+  //       countOpenedCells = 0;
+  //       setRealGameFieldBackEnd([...tempField]);
+  //       alert("You have found 2 matching Cards!");
+  //     } else if (openedCells.a !== openedCells.b && countOpenedCells !== 1) {
+  //       const tempField = [...l];
+  //       setIsMachedPair("PAIR NOT FOUND!!!");
+  //       setTimeout(function () {
+  //         if (
+  //           tempField[openedCells.ha][openedCells.wa].isGuessed === false &&
+  //           tempField[openedCells.hb][openedCells.wb].isGuessed === false
+  //         ) {
+  //           tempField[openedCells.ha][openedCells.wa].isOpened = false;
+  //           tempField[openedCells.hb][openedCells.wb].isOpened = false;
+  //         }
+  //         openedCells = { a: 9999, b: 9999, ha: 0, wa: 0, hb: 0, wb: 0 };
+  //         setRealGameFieldBackEnd([...tempField]);
+  //         countOpenedCells = 0;
+  //       }, 2000);
+  //     }
+  //   }
+  // }
   // console.log("countOpenedCells", countOpenedCells);
 }
