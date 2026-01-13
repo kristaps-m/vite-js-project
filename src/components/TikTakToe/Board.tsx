f0a60b16dae88905e8892b7c0fa5cc59d3c12c75
Kristaps M <kmitins@lansdownepayments.com>
2026-01-13 16:36:11 +0200

    Create basic click logic with currentPlayer change

--- src/components/TikTakToe/Board.tsx
+++ src/components/TikTakToe/Board.tsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cell from "./Cell";
 import "./Board.css";
 
 export default function Board() {
-  const [board, setBoard] = useState([[<Cell />, <Cell />, <Cell />], [<Cell />, <Cell />, <Cell />], [<Cell />, <Cell />, <Cell />]]);
+  // const [board, setBoard] = useState([[<Cell />, <Cell />, <Cell />], [<Cell />, <Cell />, <Cell />], [<Cell />, <Cell />, <Cell />]]);
+  // const board = [[<Cell x={0} y={0} />, <Cell x={1} y={0} />, <Cell x={2} y={0} />], [<Cell x={0} y={1} />, <Cell x={1} y={1} />, <Cell x={2} y={1} />], [<Cell x={0} y={2} />, <Cell x={1} y={2} />, <Cell x={2} y={2}  />]];
+	const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">("X");
+  const board = [];
+  for (let y = 0; y < 3; y++) {
+    const row = [];
+    for (let x = 0; x < 3; x++) {
+      row.push(<Cell x={x} y={y} setCurrentPlayer={setCurrentPlayer} currentPlayer={currentPlayer} />);
+    }
+    board.push(row);
+  }
+  // useEffect(() => {
+  //   console.log("Board updated:", board);
+  // }, [board]);
 
   return (
     <div className="board-grid">
-      {board.map((row) => {
+      {board.map((row, rowIndex) => {
         return row.map((cell, colIndex) => {
-          return <div key={colIndex} className="cell">{cell}</div>;
+          return cell;
+          // return <div key={`${rowIndex}-${colIndex}`} className="cell">{cell}</div>;
         });
       })}
     </div>
--- src/components/TikTakToe/Cell.tsx
+++ src/components/TikTakToe/Cell.tsx
@@ -1,7 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function Cell() {
-	// const [cell, setCell] = useState<"X" | "O" | null>(null);
+interface CellProps {
+	x: number;
+	y: number;
+	currentPlayer: "X" | "O";
+	setCurrentPlayer: React.Dispatch<React.SetStateAction<"X" | "O">>;
+}
 
-  return (<h1>O</h1>);
+export default function Cell({x: a, y: b, currentPlayer, setCurrentPlayer}: CellProps) {
+	const [cell, setCell] = useState<"X" | "O" | null>(null);
+	// const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">("X");
+	// console.log(cell);
+	const handleClick = () => {
+		console.log(currentPlayer);
+		if (cell === null) {
+			setCell(currentPlayer);
+		}
+		//  else if (cell === "X") {
+		// 	setCell("O");
+		// } else {
+		// 	setCell(null);
+		// }
+
+		setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+		// console.log(cell);
+	};
+
+	// useEffect(() => {
+	// 	console.log("Cell updated:", cell);
+	// }, [cell]);
+
+  // return (<h1 onClick={() =>setCell("X")}>{cell}</h1>);
+  return (<div className="cell"onClick={handleClick} key={`${a}-${b}`}>{cell}</div>);
 }
